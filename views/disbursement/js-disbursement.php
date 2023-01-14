48a0e99b541650e1248a150a8d977696e09cfac9
Santringoding <santringodingid@gmail.com>
2023-01-15 05:40:23 +0700

    Bug fixing in DisbursementModel

--- views/disbursement/js-disbursement.php
+++ views/disbursement/js-disbursement.php
@@ -45,14 +45,52 @@
     }
 
     const loadRecap = () => {
+        let start = $('#start-date').val()
+        let end = $('#end-date').val()
         $.ajax({
             url: '<?= base_url() ?>disbursement/loadrecap',
             method: 'POST',
+            data: {
+                start,
+                end
+            },
             success: function(res) {
                 $('#show-recap').html(res)
             }
         })
     }
+
+    $('#reservation').daterangepicker({
+        ranges: {
+            'Hari ini': [moment(), moment()],
+            'Kemarin': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+            '7 hari terakhir': [moment().subtract(6, 'days'), moment()],
+            '30 hari terakhir': [moment().subtract(29, 'days'), moment()],
+            'Bulan ini': [moment().startOf('month'), moment().endOf('month')],
+            'Bulan lalu': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+        },
+        autoUpdateInput: false,
+        locale: {
+            cancelLabel: 'Reset',
+            applyLabel: 'Terapkan'
+        }
+    })
+
+    $('#reservation').on('apply.daterangepicker', function(ev, picker) {
+        $(this).val('').attr('placeholder', picker.startDate.format('DD/MM/YYYY') + ' - ' + picker.endDate.format('DD/MM/YYYY'));
+        $('#start-date').val(picker.startDate.format('YYYY-MM-DD'))
+        $('#end-date').val(picker.endDate.format('YYYY-MM-DD'))
+
+        loadRecap()
+    });
+
+    $('#reservation').on('cancel.daterangepicker', function(ev, picker) {
+        $(this).attr('placeholder', 'Semua waktu').val('');
+        $('#start-date').val('')
+        $('#end-date').val('')
+
+        loadRecap()
+    });
 
     $('.indonesian-currency').autoNumeric('init', {
         aSep: '.',
